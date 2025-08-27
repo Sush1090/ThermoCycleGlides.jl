c1af5ca4c535f0b88070ef19cdf2f6095be2132f
Sush1090 <sushrut1090@gmail.com>
2025-08-27 14:54:43 +0200

    added tests for NL solver

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using ThermoCycleGlides, Clapeyron
+using ThermoCycleGlides, Clapeyron, LinearAlgebra
 using Test
 
 
@@ -34,5 +34,29 @@ end
         h2= ThermoCycleGlides.isentropic_expander(p1,p2,1.0,h1,z1,fluid_model)
         s2 = Clapeyron.PH.entropy(fluid_model,p2,h2,z1)
         @test isapprox(s1,s2,atol=1e-5)
+    end
+end
+
+@testset "ORC - mixture Non-linear solver" begin
+    fluid = cPR(["propane","butane"],idealmodel = ReidIdeal);
+    T_evap_out = 310
+    z = [0.6157894736842106, 9.384210526315789]
+    ΔT_sh = 11.473684210526315 
+    _orc_ = ORC(fluid=fluid, z=z, T_evap_in=330, T_evap_out=T_evap_out, T_cond_in=270, T_cond_out=280, η_pump=0.75, η_expander=0.75, pp_evap=3, pp_cond=3, ΔT_sc=2.0, ΔT_sh=ΔT_sh)
+    sol,res = solve(_orc_,N = 30)
+    @test norm(res) < 1e-3
+end
+
+
+
+@testset "Pure - fluids - easy ORC NL solver" begin
+    for fluid in fluids_test
+        fluid_model = cPR(fluid,idealmodel = ReidIdeal)
+        z = [1.0]
+        T_evap_out = 310
+        ΔT_sh = 5.0 
+        _orc_ = ORC(fluid=fluid_model, z=z, T_evap_in=330, T_evap_out=T_evap_out, T_cond_in=270, T_cond_out=280, η_pump=0.75, η_expander=0.75, pp_evap=3, pp_cond=3, ΔT_sc=2.0, ΔT_sh=ΔT_sh)
+        sol,res = solve(_orc_,N = 30)
+        @test norm(res) < 1e-3
     end
 end
