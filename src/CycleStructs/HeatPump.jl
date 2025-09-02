3358f9c48372a4ba071f5a9936fd07577c9cf604
Sush1090 <sushrut1090@gmail.com>
2025-09-02 15:42:19 +0200

    added Heat pump  pure fluid model

--- src/CycleStructs/HeatPump.jl
+++ src/CycleStructs/HeatPump.jl
@@ -115,6 +115,9 @@ returns the residues of the pinch points for `HeatPump`
 """
 function F(prob::HeatPump,x::AbstractVector{T};N::Int64) where {T<:Real}
     @assert length(x) == 2 "x must be a vector of length 2"
+    if length(prob.fluid.components) == 1
+        return F_pure(prob,x)
+    end
     # @show x
     p_evap,p_cond = x .* 101325 # convert to Pa
     T_evap_out = dew_temperature(prob.fluid, p_evap, prob.z)[1] + prob.ΔT_sh 
@@ -149,7 +152,36 @@ function F(prob::HeatPump,x::AbstractVector{T};N::Int64) where {T<:Real}
     return [ΔTpp_evap, ΔTpp_cond]
 end
 
+function F_pure(prob::HeatPump,x::AbstractVector{T}) where {T<:Real}
+    @assert length(x) == 2 "x must be a vector of length 2"
+    p_evap,p_cond = x .* 101325 # convert to Pa
+    T_sat_evap = saturation_temperature(prob.fluid,p_evap)[1]
+    T_sat_cond = saturation_temperature(prob.fluid,p_cond)[1]
+    T_evap_out = T_sat_evap + prob.ΔT_sh
+    h_evap_out = Clapeyron.enthalpy(prob.fluid, p_evap, T_evap_out, prob.z)
+    h_comp_in = h_evap_out;
+    h_comp_out = ThermoCycleGlides.isentropic_compressor(p_evap, p_cond, prob.η_comp, h_comp_in, prob.z, prob.fluid)
+    T_cond_out = T_sat_cond - prob.ΔT_sc
+    h_cond_out = Clapeyron.enthalpy(prob.fluid, p_cond, T_cond_out, prob.z)
+    h_cond_in = h_comp_out
+    h_cond_vapour = Clapeyron.enthalpy(prob.fluid, p_cond, T_sat_cond, prob.z,phase =:vapour)
+    h_cond_liquid = Clapeyron.enthalpy(prob.fluid, p_cond, T_sat_cond, prob.z,phase =:liquid)
+    h_cond_array = [h_cond_in,h_cond_vapour,h_cond_liquid,h_cond_out]
+    T_cond_array = Clapeyron.PH.temperature.(prob.fluid,p_cond,h_cond_array,prob.z)
+    T_cond_sf_f(h) = prob.T_cond_out - (h_cond_in - h)*(prob.T_cond_out - prob.T_cond_in)/(h_cond_in - h_cond_out)
+    ΔT_cond = minimum(T_cond_array .- T_cond_sf_f.(h_cond_array)) - prob.pp_cond
+    @show T_cond_array,T_cond_sf_f.(h_cond_array)
+    h_valve_in = h_cond_out;
+    h_valve_out = h_valve_in # isenthalpic expansion
 
+    h_evap_in = h_valve_out
+    h_evap_sat_vapour = Clapeyron.enthalpy(prob.fluid, p_cond, T_sat_evap, prob.z,phase =:vapour)
+    h_evap_array = reverse([h_evap_in,h_evap_sat_vapour,h_evap_out])
+    T_evap_array = Clapeyron.PH.temperature.(prob.fluid,p_evap,h_evap_array,prob.z)
+    T_evap_sf_f(h) = prob.T_evap_in - (h_evap_out - h)*(prob.T_evap_in - prob.T_evap_out)/(h_evap_out - h_evap_in)
+    ΔT_evap = minimum(T_evap_sf_f.(h_evap_array) .- T_evap_array) - prob.pp_evap
+    [ΔT_cond,ΔT_evap]
+end
 
 """
 Function that gives specific power ratings for HP by fixing outlet power of compressor to equal 1.
