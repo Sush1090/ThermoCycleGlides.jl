74c462e86980acaca057a4f3cae1f2c17ea34edd
Sush1090 <sushrut1090@gmail.com>
2025-09-02 17:53:39 +0200

    chnaged isentropic compression to fix in ph not pt

--- src/thermoextensions/utils.jl
+++ src/thermoextensions/utils.jl
@@ -4,9 +4,10 @@ function isentropic_compressor(p_in::T1, p_out::T2, η_isen::T3, h_in::T4, z::Ab
     s_isen = Clapeyron.PH.entropy(fluid, p_in, h_in, z)
     h_isen = Clapeyron.PS.enthalpy(fluid, p_out, s_isen, z)
     ha =  h_in + (h_isen - h_in) / η_isen
-     T_out = Clapeyron.PH.temperature(fluid,p_out,ha,z)
+    #  T_out = Clapeyron.PH.temperature(fluid,p_out,ha,z)
      T_dew = dew_temperature(fluid,p_out,z)[1]
-     if T_out < T_dew
+     h_dew = enthalpy(fluid,p_out,T_dew)
+     if ha < h_dew
         @warn "Fixing outlet of compressor at saturation temperature"
         return enthalpy(fluid,p_out,T_dew,z,phase = :vapour)
     end
