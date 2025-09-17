fcee1e13ecc7b1b6c002585772382978b63bf384
Sush1090 <sushrut1090@gmail.com>
2025-09-17 14:47:27 +0200

    extend `IHEX_Q` for AD

--- src/thermoextensions/utils.jl
+++ src/thermoextensions/utils.jl
@@ -36,8 +36,8 @@ function isentropic_expander(p_in::T1,p_out::T2,η_isen::T3,h_in::T4,z::Abstract
     return h_out
 end
 
-function IHEX_Q(fluid::EoSModel,ϵ::T,T_in_left::T,p_in_left::T,T_in_right::T,p_in_right::T,z::AbstractVector{T}) where T<:Real
-    @assert T_in_left >= T_in_right "Function assumse left fluid is hotter"
+function IHEX_Q(fluid::EoSModel,ϵ::T1,T_in_left::T2,p_in_left::T3,T_in_right::T4,p_in_right::T5,z::AbstractVector{T}) where {T<:Real,T1<:Real,T2<:Real,T3<:Real,T4<:Real,T5<:Real}
+    T_in_left <= T_in_right && @warn  "Function assumse left fluid is hotter. This didn't happen. Now heat transfer will be negative."
     c_in_left = Clapeyron.isobaric_heat_capacity(fluid,p_in_left,T_in_left,z)
     c_in_right = Clapeyron.isobaric_heat_capacity(fluid,p_in_right,T_in_right,z)
     C = min(c_in_left,c_in_right)
