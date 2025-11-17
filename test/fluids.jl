89475f6c21ab96889c1a361bccacd2989cb9a7e2
Sush1090 <sushrut1090@gmail.com>
2025-11-17 14:46:47 +0100

    update test

--- test/fluids.jl
+++ test/fluids.jl
@@ -3,4 +3,4 @@
 fluids_test = ["dimethyl ether","isobutane","Butene","Neopentane","isopentane",
             "pentane","hexane","acetone","cyclopentane","heptane","cyclohexane","benzene","propane"]
 
- const  eos_ = [cPR,KU,SRK] # list of EoS models to test
+const  eos_ = [cPR,KU,SRK] # list of EoS models to test
