145573ee8e22b5ecb20759ef27488bfba1c410a3
Sush1090 <sushrut1090@gmail.com>
2026-02-19 14:17:32 +0100

    add basic data struct for component performance map

---
+++ src/DataMap/DataMap.jl
@@ -0,0 +1,18 @@
+abstract type ThermoCycleGlidesData end
+
+struct CompressorData <:ThermoCycleGlidesData 
+    p_ratio::Vector{Float64}
+    eff::Vector{Float64}
+end
+
+struct ExpanderData <:ThermoCycleGlidesData 
+    p_ratio::Vector{Float64}
+    eff::Vector{Float64}
+end
+
+
+function generate_map(data::ThermoCycleGlidesData,order = 3)
+    poly = fit(data.p_ratio, data.eff, order)
+    η_fun(p) = clamp(poly(p), 0.0, 1.0)
+    return η_fun
+end
