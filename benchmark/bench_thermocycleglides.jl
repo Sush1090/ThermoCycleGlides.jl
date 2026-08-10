4da1c4f744349c30a265b7cab96b48a9594eb46e
Sush1090 <sushrut1090@gmail.com>
2026-08-10 13:23:56 +0200

    update benchmarks to CarnotBench
    Warning for project.toml - it still uses thermocycleglides

--- benchmark/bench_thermocycleglides.jl
+++ benchmark/bench_thermocycleglides.jl
@@ -1,17 +1,47 @@
-module ThermoCycleBench
-    using BenchmarkTools, Clapeyron, ThermoCycleGlides
-    
-    const p = 101325.0
-    const h = collect(range(-1,1,100));
-    const fluid  = cPR(["Propane","R134a"],idealmodel = ReidIdeal); 
-    const z = [1.0,1.0]
-    const p_out = 5*p
-    const η_isen = 0.8
-    f_compression(x) = ThermoCycleGlides.isentropic_compressor(p,p_out,η_isen,x,z,fluid)
-    suite = BenchmarkGroup()
-    suite["utils"] = BenchmarkGroup()
-    suite["utils"]["compressor"] = @benchmarkable f_compression.(x) setup=(x = h)
+module CarnotBench
+
+using BenchmarkTools
+using Clapeyron
+using Carnot
+
+# -------------------------
+# Constants
+# -------------------------
+const p = 2 * 101325.0
+const p_out = 5 * p
+const p_out_expander = p / 5
+const η_isen = 0.8
+
+const fluid = cPR(["Propane", "butane"], idealmodel = ReidIdeal)
+const z = [1.0, 1.0]
+
+const h = collect(range(-1, 1, length = 100))
+    x = copy(h)
+# -------------------------
+# Functions
+# -------------------------
+f_compression(x) =
+    ThermoCycleGlides.isentropic_compressor(p, p_out, η_isen, x, z, fluid)
+
+f_expansion(x) =
+    ThermoCycleGlides.isentropic_expander(p, p_out_expander, η_isen, x, z, fluid)
+
+# -------------------------
+# Benchmark suite
+# # -------------------------
+suite = BenchmarkGroup()
+
+suite["utils"] = BenchmarkGroup()
+
+suite["utils"]["compressor"] = @benchmarkable begin
+    f_compression.($x)
 end
 
-ThermoCycleBench.suite
+suite["utils"]["expander"] =
+    @benchmarkable f_expansion.(x) setup=(x = copy(h),)
 
+
+end
+
+# return benchmark suite
+CarnotBench.suite
